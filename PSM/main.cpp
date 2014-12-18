d2ead0be7da2e4d728950abf8fb5db9571675d44
KimBongSeok <csteacher@KimBongSeokui-MacBook-Air.local>
2014-12-18 14:12:10 +0900

    Initial Commit

---
+++ PSM/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  PSM
+//
+//  Created by KimBongSeok on 2014. 12. 18..
+//  Copyright (c) 2014년 KimBongSeok. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
