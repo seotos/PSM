909d36a6b5c5f581d4504dd170f1c073f6d4e138
seotos <seotosna@naver.com>
2016-04-22 10:51:38 +0900

    ggg

--- GitTest/judgeon_4011.cpp
+++ GitTest/judgeon_4011.cpp
@@ -6,12 +6,9 @@ KOI 2005 지역초등 2
 #include<stdio.h>
 int main(){
 	int a,b;
-	 scanf("%d%d",&a,&b);
-		
-
-	
+	 scanf("%d%d",&a,&b);	
 	printf("%d\n%d\n%d\n%d",a*(b%10),a*((b/10)%10),a*((b/100)%10),a*b);
-	
+
 
 	return 0;
 }
