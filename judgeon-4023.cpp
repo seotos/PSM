1c6e7e8b205941103af94d0672e4172786dd21e4
KBS <seotosna@naver.com>
2016-06-28 11:36:25 +0900

    영역구하기

--- judgeon-4023.cpp
+++ judgeon-4023.cpp
@@ -5,5 +5,28 @@
 //  Created by KimBongSeok on 2016. 6. 23..
 //  Copyright © 2016년 KimBongSeok. All rights reserved.
 //
-
+/*
+ 5 7 3
+ 0 2 4 4
+ 1 1 2 5
+ 4 0 6 2
+ */
 #include <stdio.h>
+#include<algorithm>
+using namespace std;
+int m,n,k;
+int ansCnt[100],fcnt;
+int map[120][120];
+struct RECT{
+    int x1,y1,x2,y2;
+};
+struct POINT{
+    int x, y;
+};
+int dx[]={-1,0,1,0},dy[]={0,-1,0,1};
+void back(POINT t){
+
+}
+int main(){
+    
+}
