24c8d72139253928f2a1f837ced7e006c54ef9b3
KBS <seotosna@naver.com>
2016-07-04 10:06:34 +0900

    koistudy-1473   USACO 2016 jan bronze Problem number one Promotion Counting

---
+++ judgeon-4045.cpp
@@ -0,0 +1,30 @@
+//
+//  judgeon-4045.cpp
+//  PSM
+//
+//  Created by KBS on 2016. 7. 1..
+//  Copyright © 2016년 KBS All rights reserved.
+//
+/*
+ 정사각형 체스판의 크기 N
+ 장애물의 개수 M
+ N은 100이하의 자연수이고 M은 음이 아닌 정수
+ 장애물의 위치가 주어진다. 위에서부터 몇 번째 행인지와 왼쪽부터 몇 번째 열
+ 
+ 5
+ 1
+ 3 3
+ 
+ 10
+ 
+ */
+#include <stdio.h>
+
+// 놓을수 있는지 없는지 체크
+// 놓을 위치 사이에 장애물이 있는지 확인
+int isOk(int y, int x){
+    
+}
+int main(){
+    
+}
---
+++ koistudy-1473.cpp
@@ -0,0 +1,37 @@
+//
+//  koistudy-1473.cpp
+//  PSM
+//
+//  Created by KBS on 2016. 7. 4..
+//  Copyright © 2016년 KBS. All rights reserved.
+//
+/*
+ 1 2
+ 1 1
+ 1 1
+ 1 2
+ 
+ 1
+ 1
+ 1
+ */
+
+#include <stdio.h>
+#define abs(a) ((a)>0?(a):-(a))
+struct C{
+    int old, after;
+}d[4];
+int main(){
+    for(int i=0;i<4;i++){
+        scanf("%d%d",&d[i].old,&d[i].after);
+    }
+    int gap[5]={};
+    int tmp=0;
+    for(int i=3;i>=0;i--){
+        gap[i]=abs((d[i].after+tmp)-d[i].old);
+        tmp=gap[i];
+    }
+    for(int i=1;i<=3;i++)
+        printf("%d\n",gap[i]);
+    return 0;
+}
